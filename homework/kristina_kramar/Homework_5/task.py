3c3fabd1c4bed09eb8c220f4fdea8a5010e5f665
kristina.kramar <k.kpamap@gmail.com>
2026-01-31 12:51:20 +0100

    lint fixed

--- homework/kristina_kramar/Homework_5/task.py
+++ homework/kristina_kramar/Homework_5/task.py
@@ -5,13 +5,13 @@ str1 = 'результат операции: 42'
 str2 = 'результат операции: 514'
 str3 = 'результат работы программы: 9'
 index_str1 = str1.index(':')
-str1_number = int(str1[index_str1+1:].strip())
+str1_number = int(str1[index_str1 + 1:].strip())
 print(str1_number + 10)
 index_str2 = str2.index(':')
-str2_number = int(str2[index_str2+1:].strip())
+str2_number = int(str2[index_str2 + 1:].strip())
 print(str2_number + 10)
 index_str3 = str3.index(':')
-str3_number = int(str3[index_str3+1:].strip())
+str3_number = int(str3[index_str3 + 1:].strip())
 print(str3_number + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']

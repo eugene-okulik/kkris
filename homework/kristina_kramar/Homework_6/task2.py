c0ad5dd618bf1214edd6735cb0489b498e90abb9
kristina.kramar <k.kpamap@gmail.com>
2026-02-03 10:56:37 +0100

    homework_6 done

---
+++ homework/kristina_kramar/Homework_6/task2.py
@@ -0,0 +1,15 @@
+numbers = list(range(1, 101))
+
+for number in numbers:
+    if number % 3 == 0:
+        if number % 5 == 0:
+            print('FuzzBuzz')
+        else:
+            print('Fuzz')
+    elif number % 5 == 0:
+        if number % 3 == 0:
+            print('FuzzBuzz')
+        else:
+            print('Buzz')
+    else:
+        print(number)

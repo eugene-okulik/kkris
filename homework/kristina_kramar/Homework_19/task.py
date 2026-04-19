16530f3daa86b8f7e03499d9013aaf675fa30448
kristina.kramar <k.kpamap@gmail.com>
2026-04-19 11:46:42 +0200

    lint fixed

--- homework/kristina_kramar/Homework_19/task.py
+++ homework/kristina_kramar/Homework_19/task.py
@@ -1,8 +1,10 @@
 import requests
 
+
 def all_objects():
     response = requests.get('http://objapi.course.qa-practice.com/object').json()
     assert len(response) == 1, 'Not all objects returned'
+
 
 all_objects()
 
@@ -11,6 +13,7 @@ def one_object():
     object_id = 1
     response = requests.get(f'http://objapi.course.qa-practice.com/object/{object_id}').json()
     assert response['id'] == object_id, 'Wrong object id returned'
+
 
 one_object()
 
@@ -28,6 +31,7 @@ def post_object():
     assert response.status_code == 201, 'Wrong status code returned'
     assert response.json()['name'] == 'My object', 'Wrong body name returned'
 
+
 post_object()
 
 
@@ -43,6 +47,7 @@ def put_object():
     response = requests.put('http://objapi.course.qa-practice.com/object/1', json=body, headers=header)
     assert response.json()['name'] == 'My object-UPD', 'Wrong body name returned'
 
+
 put_object()
 
 
@@ -54,11 +59,13 @@ def patch_object():
     response = requests.patch('http://objapi.course.qa-practice.com/object/1', json=body, headers=header)
     assert response.json()['name'] == 'My object-UPDUPD', 'Wrong body name returned'
 
+
 patch_object()
 
 
 def delete_object():
     response = requests.delete('http://objapi.course.qa-practice.com/object/1')
     assert response.status_code == 200, 'Wrong status code returned'
+
 
 delete_object()
